abead85bd00ade032a24f7848b5d3155aa04a7e3
Pramod2006 <124419175+Pramod2006@users.noreply.github.com>
2023-02-11 19:42:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,18 +31,18 @@ global.language = english //change indonesia to english if you don't understand 
 
 // Other
 // Other
-global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
+global.botname = "Alphabot-Md Mihiruk" //namabot kalian
+global.ownername = "Mihiruk" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@spchathuofficial8091" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
-global.email = "mdsandaru2@gmail.com" //bebas
+global.email = "chathusp6@gmail.com" //bebas
 global.region = "Buddhist" //bebas
 global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
-global.ownernomerr = "+94764497078" //nmr wa kalian
+global.premium = ['94761091578'] //premium user
+global.owner = ["761091578", "94761091578"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "761091578" // nomor wa kalian
+global.ownernomerr = "+94761091578" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
